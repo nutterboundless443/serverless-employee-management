5a4d80ca3882c665c009b787a0f899155f926309
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-06 13:58:15 -0500

    Add a new employee route with a placeholder response.

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/employees', (req, res) => {
   res.send('Fetching employee information');
 });
 
+// New route for adding an employee
+app.post('/employees', (req, res) => {
+  res.send('Adding new employee');
+});
+
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
